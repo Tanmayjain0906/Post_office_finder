6ddc569a6240734d3121c4dbe132ff0bf64d2622
Tanmayjain0906 <tanmayjain983@gmail.com>
2023-10-05 13:03:01 +0530

    post

---
+++ README.md
@@ -0,0 +1,3 @@
+# Post_office_finder
+please allow location permission to go further.
+If you denied location on any page it will redirect to a first page.
--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-if(localStorage.getItem("location"))
-{
-    window.location.href = "./get.html";
-}
+
 
 navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
-function onSuccess(position)
-{
-    let {latitude, longitude} = position.coords;
+function onSuccess(position) {
+    let { latitude, longitude } = position.coords;
     let location = {
         lat: latitude,
         long: longitude
     }
-    localStorage.setItem("location",JSON.stringify(location));
+    localStorage.setItem("location", JSON.stringify(location));
     window.location.href = "./get.html";
 }
 
